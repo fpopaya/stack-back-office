3218501c4ed92cf1038cbfd1a2d0db2b0d868771
Fidel <fidel.popayan@bci.cl>
2024-09-17 11:05:24 -0300

    Update dependencias

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://crudbb.netlify.app/.netlify/functions/server";
+const BASE_URL = 'https://crudbb.netlify.app/.netlify/functions/server';
 export const api = axios.create({
   baseURL: BASE_URL,
 });
